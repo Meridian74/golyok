a606c69f3d9d70ad7ae48b8c3d35410e0058ce05
Meridian74 <meridian@freemail.hu>
2019-02-27 21:40:45 +0100

    feat: sok golyó véletlenszerűen mozogva

--- sketch.js
+++ sketch.js
@@ -1,57 +1,63 @@
-var screenWidth = 640;
-var screenHeight = 400;
-var r = 0;
-var x = 0;
-var y = 0;
-var dX = 0;
-var dY = 0;
-var step = 0;
+
+var screenWidth = 800;
+var screenHeight = 600;
+var tombhossz = 100;
+var speed = 5;
+var r = new Array([tombhossz]);
+var x = new Array([tombhossz]);
+var y = new Array([tombhossz]);
+var dX = new Array([tombhossz]);
+var dY = new Array([tombhossz]);
+var step = new Array([tombhossz]);
 
 function setup() {
   createCanvas(screenWidth, screenHeight);
-  background(50);
-  var x = 0.0 + random(screenWidth - r/2);
-  var y = 0.0 + random(screenHeight - r/2);
-  dX = random(-8,8);
-  dY = random(-8,8);
-  step = parseInt(random(20));
-  r = 20;
+  for (i = 0; i < tombhossz; i++){
+    r[i] = parseInt(random(20,20));
+    x[i] = 0 + random(screenWidth - (r[i] / 2));
+    y[i] = 0 + random(screenHeight - (r[i] / 2));
+    dX[i] = random(-speed, speed);
+    dY[i] = random(-speed, speed);
+    step[i] = parseInt(random(50));
+  }
 }
 
 function draw() {
-  
-
+  background(50);
   fill(255);
-  ellipse(x, y, r, r);
-
-  x = x + dX;
-  y = y + dY;
-  if (step > 0) {
-    step = step - 1;
+  stroke(100);
+  for(j = 0; j < 2; j++){
+    for(i = 0; i < tombhossz; i++) {
+      ellipse(x[i], y[i], r[i], r[i]);
+      x[i] = x[i] + dX[i];
+      y[i] = y[i] + dY[i];
+      
+      if (step[i] > 0) {
+        step[i]--;
+      }
+      else {
+        step[i] = parseInt(random(30,50));
+        dX[i] = random(-speed, speed);
+        dY[i] = random(-speed, speed);
+      }
+      
+      if (x[i] < (r[i] / 2)) {
+        x[i] = r[i] / 2;
+        dX[i] = -1 * dX[i];
+      }
+      else if (x[i] > (screenWidth - r[i] / 2)) {
+        x[i] = screenWidth - r[i] / 2;
+        dX[i] = -1 * dX[i];
+      }
+      
+      if (y[i] < (r[i] / 2)) {
+        y[i] = r[i] / 2;
+        dY[i] = -1 * dY[i];
+      }
+      else if (y[i] > (screenHeight - r[i] / 2)) {
+        y[i] = screenHeight - r[i] / 2;
+        dY[i] = -1 * dY[i];
+      }
+    }
   }
-  else {
-    step = parseInt(random(20));
-    dX = random(-8,8);
-    dY = random(-8,8);
-  }
-
-  if (x < (r/2)) {
-    x = r/2 - 1;
-    dX = dX * -1;
-  }
-  else if (x > (screenWidth - r/2)) {
-    x = screenWidth - r/2;
-    dX = dX * -1;
-  }
-
-  if (y < (r/2)) {
-    y = r/2;
-    dY = dY * -1;
-  }
-  else if (y > (screenHeight - r/2)) {
-    y = screenHeight - r/2;
-    dY = dY * -1;
-  }
-  
 }
-
