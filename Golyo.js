efb18ca6ba5a8eeee37b5c506436d319d21cea3e
Meridian74 <meridian@freemail.hu>
2019-03-03 01:29:53 +0100

    feat: a golyók ütközései megvalósítva

--- Golyo.js
+++ Golyo.js
@@ -4,20 +4,22 @@ class Golyo {
       this.x = random(SCREEN_W);                   // a golyó kezdeti X pozíciója
       this.y = random(SCREEN_H);                   // a golyó kezdeti Y pocíciója
       this.sebesseg = random(SPEED);               // a golyó mozgási sebességének nagysága
-      this.irany = random(TWO_PI);                 // egy 0-360°-os véletlenszerű irány
+      this.irany = random(-PI, PI);                 // egy 0-360°-os véletlenszerű irány
       this.dX = this.sebesseg * cos(this.irany);   // X irányú elmozdulás nagysága
-      this.dY = this.sebesseg * sin(this.irany);   // Y irányú elmozdulás nagysága
+      this.dY = this.sebesseg *-sin(this.irany);   // Y irányú elmozdulás nagysága
    }
 
    // golyó kirajzolása
    display() {
-      ellipse(this.x, this.y, this.r, this.r);
+      stroke(100);
+      fill(255);
+      ellipse(this.x, this.y, this.r);
    }
 
    // golyó mozgatása 
    move() {
-      this.x += this.dX;
-      this.y += this.dY;
+      this.x = this.x + this.dX;
+      this.y = this.y + this.dY;
    }
 
    // képernyő határával való találkozás vizsgálata
@@ -25,26 +27,37 @@ class Golyo {
       if (this.x < this.r / 2) {
          this.x = this.r / 2;
          this.dX = -this.dX;
+         this.irany = createVector(this.dX, this.dY).heading();
+         this.iranySzepites();
       }
       else if (this.x > SCREEN_W - this.r / 2) {
          this.x = SCREEN_W - this.r / 2;
          this.dX = -this.dX;
+         this.irany = createVector(this.dX, this.dY).heading();
+         this.iranySzepites();
       }
          
       if (this.y < this.r / 2) {
          this.y = this.r / 2;
          this.dY = -this.dY;
+         this.irany = createVector(this.dX, this.dY).heading();
+         this.iranySzepites();
       }
       else if (this.y > SCREEN_H - this.r / 2) {
          this.y = SCREEN_H - this.r/2;
          this.dY = -this.dY;
+         this.irany = createVector(this.dX, this.dY).heading();
+         this.iranySzepites();
       }
    }
 
-   collision(ss) {
-      this.sebesseg = ss;
-      this.irany = random(TWO_PI);     
-      this.dX = this.sebesseg * cos(this.irany);
-      this.dY = this.sebesseg * sin(this.irany);
+   // A irányszög korrigálása 0 és 2PI közé
+   iranySzepites() {
+      if (this.irany > PI) {
+         this.irany = this.irany - TWO_PI;
+      }
+      else if (this.irany < -PI) {
+         this.irany = this.irany + TWO_PI;
+      }
    }
 }
--- sketch.js
+++ sketch.js
@@ -1,37 +1,58 @@
-SCREEN_W = 800;
-SCREEN_H = 600;
-MENNYI = 100;           // a golyók mennyisége
-SPEED = 10;              // a X/Y irányú maximális elmozdulási érték
+SCREEN_W = 600;
+SCREEN_H = 400;
+MENNYI = 30;             // a golyók mennyisége
+SPEED = 5;              // a X/Y irányú maximális elmozdulási érték
 ATMERO = 20;            // a golyók átmérője
 var golyok = new Array(MENNYI);
-var ujSebesseg;
+var i;
+var j;
+var tX;
+var tY;
+var tempCsere;
+
 
 function setup() {
    createCanvas(SCREEN_W, SCREEN_H);
    for (i = 0; i < MENNYI; i++) {
-      golyok[i] = new Golyo(ATMERO);
+      golyok[i] = new Golyo(ATMERO, i + 100);
    }
 }
 
 
 function draw() {
    background(50);
-   fill(255);
-   stroke(100);
-   for (i = 0; i < MENNYI; i++) {
-      golyok[i].display();
-      golyok[i].move();
-      golyok[i].checkBorder();
-   }
+
+   // ütközés detekálás és ennek okán mozgási irányváltás
    for (i = 0; i < MENNYI - 1; i++) {
-      for (j = 1; j < MENNYI; j++) {
+      for (j = i + 1; j < MENNYI; j++) {
+         // a 2 golyó közötti távolságot megvizsgáljuk,
+         // és ha a távolság kisebb, mint a golyó átmérője akkor a két golyó ütközik!
          tavolsag = dist(golyok[i].x, golyok[i].y, golyok[j].x, golyok[j].y);
-         
-         if (tavolsag <= ATMERO * 2) {
-            ujSebesseg = (golyok[i].sebesseg + golyok[j].sebesseg) / 2;
-            golyok[i].collision(ujSebesseg);
-            golyok[j].collision(ujSebesseg);
+         if (tavolsag <= ATMERO) {
+            tX = golyok[i].dX - golyok[j].dX;
+            tY = golyok[i].dY - golyok[j].dY;
+            if (tX > tY) {
+               tempCsere = golyok[i].dX;
+               golyok[i].dX = golyok[j].dX;
+               golyok[j].dX = tempCsere;
+            }
+            else {
+               tempCsere = golyok[i].dY;
+               golyok[i].dY = golyok[j].dY;
+               golyok[j].dY = tempCsere;
+            }
          }
       }
+   }
+
+   // a golyók mozgatás a következő pozicióba
+   for (i = 0; i < MENNYI; i++) {
+      golyok[i].move();
+   }
+
+   // képernyő szélén túl van-e, ha igen mozgási irányváltás - majd a golyó megjelenítése
+   for (i = 0; i < MENNYI; i++) {
+      golyok[i].checkBorder();
+      golyok[i].display();
    }
 }
